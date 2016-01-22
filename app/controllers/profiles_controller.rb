7fabfecb59a0a205c49d10ac7a8d5379f0de1a22
Christian S Paraiso <cparaiso.dev@outlook.com>
2016-01-22 03:03:56 +0000

    Add user show action and bare bones user show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,6 +18,6 @@ class ProfilesController < ApplicationController
    
    private
    def profile_params # whitelist input data from 'new' page submit action
-    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_nuber, :contact_email, :description)
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
    end
 end
