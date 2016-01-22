9ff55740f62df07afc112b54b50df0ff1827b79e
Christian S Paraiso <cparaiso.dev@outlook.com>
2016-01-22 02:30:01 +0000

    Add profiles and profiles form. Add new  and create actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,23 @@
 class ProfilesController < ApplicationController
-    
+   def new
+       # display form where a user can fill out their own profile
+       @user = User.find( params[:user_id] )
+       @profile = @user.build_profile
+   end
+   
+   def create
+       @user = User.find(params[:user_id])
+       @profile = @user.build_profile( profile_params ) # build profile based on whitelisted data passed from the new page 
+       if @profile.save
+           flash[:success] = "Profile Updated"
+           redirect_to user_path( params[:user_id] )
+       else
+           render action: :new
+       end
+   end
+   
+   private
+   def profile_params # whitelist input data from 'new' page submit action
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_nuber, :contact_email, :description)
+   end
 end
